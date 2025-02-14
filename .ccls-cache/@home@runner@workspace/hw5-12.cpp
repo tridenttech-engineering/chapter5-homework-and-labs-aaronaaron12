11c85620c32a684ef68d3966279b26ea8443a8fc
aaronaaron12 <jetsam_retrial.4p@icloud.com>
2025-02-14 14:36:42 +0000

    Submission for 5-2 through 5-12 Aaron Bloodworth

---
+++ .ccls-cache/@home@runner@workspace/hw5-12.cpp
@@ -0,0 +1,35 @@
+//hw5-12.cpp - displays the total owed
+//Created/revised by <your name> on <current date>
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	const double DISCOUNT_RATE = 0.1;
+	const double SHIP_CHG1 = 0.99;
+	const double SHIP_CHG2 = 4.99;
+	double amtOwed = 0.0;
+	char member = ' ';
+
+	cout << "Amount owed before any discount and shipping: ";
+	cin >> amtOwed;
+	cout << "Premier Club member (Y/N)?";
+	cin >> member;
+
+	if (toupper(member) == 'Y')
+		amtOwed -= amtOwed * DISCOUNT_RATE;
+	//end if
+	if {} (amtOwed >= 100.0)
+	amtOwed += SHIP_CHG1};
+	else
+	amtOwed += SHIP_CHG2;
+	//end if
+	cout << fixed << setprecision(2);
+	cout << "Amount owed after any discount and shipping: "
+			<< amtOwed;
+	
+
+	return 0;
+}	//end of main function
--- hw5-12.cpp
+++ hw5-12.cpp
@@ -7,6 +7,23 @@ using namespace std;
 
 int main()
 {
-
+	//define variables
+	double item1_Cost = 0.0;
+	double item2_Cost = 0.0;
+	double total = 0.0;
+	//enter input items
+	cout << "Enter cost of item 1: ";
+	cin >> item1_Cost;
+	cout << "Enter cost of item 2: ";
+	cin >> item2_Cost;
+	if (item1_Cost >= item2_Cost)
+		total = item1_Cost + (item2_Cost / 2);
+	else
+		total = (item1_Cost / 2) + item2_Cost;
+	//end if
+	//display total owed
+	cout << fixed << setprecision(2);
+	cout << "Total owed: " << total << endl;
+	
 	return 0;
 }	//end of main function
